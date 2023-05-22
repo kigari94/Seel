b5b3120194f3265046b23e0b48d5900749ae4f3a
kigari94 <36a8d9lf21x>
2023-05-22 16:37:53 +0200

    adding score system + UI elements

--- Unity/SimpleOSCTest/Assets/Scripts/BallMovement.cs
+++ Unity/SimpleOSCTest/Assets/Scripts/BallMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class BallMovement : MonoBehaviour
@@ -12,6 +13,8 @@ public class BallMovement : MonoBehaviour
     private int scorePlayerOne = 0;
     private int scorePlayerTwo = 0;
     private Rigidbody2D rigidBody;
+    public TextMeshProUGUI scoreTxtPlayerOne;
+    public TextMeshProUGUI scoreTxtPlayerTwo;
 
     // Start is called before the first frame update
     void Start()
@@ -29,10 +32,11 @@ public class BallMovement : MonoBehaviour
 
     public void IncreaseHitCounter()
     {
-        if(hitCounter * extraSpeed <= 0)
+        if(hitCounter * extraSpeed <= maxSpeed)
         {
             hitCounter++;
             BallMove();
+            //Debug.Log(hitCounter);
         }
     }
 
@@ -45,48 +49,69 @@ public class BallMovement : MonoBehaviour
         }
     }
 
+    public void UpdateScore(string player, int score)
+    {
+        if(player == "PlayerOne")
+        {
+            scorePlayerOne += score;
+            scoreTxtPlayerOne.text = scorePlayerOne.ToString("0000");
+        }
+        else
+        {
+            scorePlayerTwo += score;
+            scoreTxtPlayerTwo.text = scorePlayerTwo.ToString("0000");
+        }
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        string collisionObject = collision.gameObject.name;
+        string collisionObject = collision.gameObject.tag;
+        Debug.Log(collisionObject);
 
         switch (collisionObject)
         {
             case "Player":
                 IncreaseHitCounter();
                 break;
-            // ToDo: Punkte von der Y-Position der Bricks abhängig machen
             case "BrickTopOne":
                 Destroy(collision.gameObject);
-                // add points
+                UpdateScore("PlayerOne", 5);
                 // decrease Speed if y < 0 (Coroutine) oder State via Setter
                 break;
             case "BrickBottomOne":
                 Destroy(collision.gameObject);
+                UpdateScore("PlayerTwo", 5);
                 break;
             case "BrickTopTwo":
                 Destroy(collision.gameObject);
+                UpdateScore("PlayerOne", 10);
                 break;
             case "BrickBottomTwo":
                 Destroy(collision.gameObject);
+                UpdateScore("PlayerTwo", 10);
                 break;
             case "BrickTopThree":
                 Destroy(collision.gameObject);
+                UpdateScore("PlayerOne", 15);
                 break;
             case "BrickBottomThree":
                 Destroy(collision.gameObject);
+                UpdateScore("PlayerTwo", 15);
                 break;
             case "BrickTopFour":
                 Destroy(collision.gameObject);
+                UpdateScore("PlayerOne", 20);
                 break;
             case "BrickBottomFour":
                 Destroy(collision.gameObject);
+                UpdateScore("PlayerTwo", 20);
                 break;
             case "OuterWallTop":
-                scorePlayerOne += 50;
+                UpdateScore("PlayerOne", 50);
                 // end game
                 break;
             case "OuterWallBottom":
-                scorePlayerTwo += 50;
+                UpdateScore("PlayerTwo", 50);
                 // end game
                 break;
         }
