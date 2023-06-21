2291b5dab1b3935a0fbf251fd58b51aeaa659650
kigari94 <stefankulow@web.de>
2023-06-21 22:21:07 +0200

    adding particle effects

--- Unity/SimpleOSCTest/Assets/Scripts/brickActions.cs
+++ Unity/SimpleOSCTest/Assets/Scripts/brickActions.cs
@@ -3,13 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class brickActions : MonoBehaviour
-
 {
+    public ParticleSystem collisionParticleSystem;
+    public bool once = true;
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Ball")
+        if (collision.gameObject.tag == "Ball" && once)
         {
-            Destroy(gameObject);
+            var emission = collisionParticleSystem.emission;
+            var duration = collisionParticleSystem.duration;
+
+            emission.enabled = true;
+            collisionParticleSystem.Play();
+
+            once = false;
+            Invoke(nameof(DestroyObject), duration);
+            
         }
+    }
+
+    void DestroyObject()
+    {
+        Destroy(gameObject);
     }
 }
